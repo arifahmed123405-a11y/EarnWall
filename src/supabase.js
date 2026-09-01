205c29adf3baaceb88eb4799bff45daa048c3eb1
arifahmed123405-a11y <arifahmed123405@gmail.com>
2026-09-01 21:03:40 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://uajeuksurhgypsqicddr.supabase.co',
-  'sb_publishable_3M8XvNk_rLQqkEhDUgzYow_FdYs-Sb2'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhamV1a3N1cmhneXBzcWljZGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyNjg5OTUsImV4cCI6MjEwMzg0NDk5NX0.3SgiiLmlj21dJaxkzuWCK6HchTM7A_3CXL2I4lmuBdk'
 )
